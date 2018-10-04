091fa1010e49bace6c3ae49c329f384980daf2d5
DANIEL DO AMARAL SILVEIRA <daniel.silveira15@etec.sp.gov.br>
2018-10-04 17:19:56 -0300

    end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { FilterByDescription } from './filter-by-description.pipe';
+import { PokemonService } from './pokemon.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterByDescription
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PokemonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
